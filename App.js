fe563dbc1fbaaa899555a6e7b7cd1dd47d293b52
Vishnu Kommuri <vishnukommuri@gmail.com>
2025-07-06 17:16:11 -0400

    feat: scaffold Expo project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HelloStyleMate!</Text>
+      <Text>Hello StyleMate!</Text>
       <StatusBar style="auto" />
     </View>
   );

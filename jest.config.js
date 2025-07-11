7646b101463d27b7b55d12bea134e294f5f1e87e
Vishnu Kommuri <vishnukommuri@gmail.com>
2025-07-11 16:32:11 -0400

    Implement wardrobe favorites feature v3

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', './jest.setup.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|expo|@expo|@unimodules|react-native-gesture-handler|react-native-reanimated|react-native-screens|react-native-safe-area-context|react-native-web|@react-native-async-storage/async-storage))',
-  ],
-};
